17bb2c54d4b3eeb15f325a246d193f6058d190bd
Analissoares <analissoares19@gmail.com>
2025-04-20 11:17:07 -0300

    app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.set_page_config(
 
 def main():
     st.title("Mapas no Streamlit")
-    st.subheader("AULA4")
+    st.subheader("Localização de sistemas bancários")
 
     menu = ["Menu", "Mapa"]
     choice = st.sidebar.selectbox("Menu", menu)
